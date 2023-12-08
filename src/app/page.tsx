b7ce6510c652a7d0b3dd6a18097134c18a907274
radical-coder <coderradical@gmail.com>
2023-12-08 03:03:26 -0500

    Refactor product tags rendering in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,34 @@ import DataTable from './DataTable'
 import data from './data.json';
 import Image from 'next/image';
 
+const Tag = ({ item }: { item: string }) => (
+  <div className='m-1 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500 '>
+    {item}
+  </div>
+);
+
 
 export default function Home() {
   let product = data[0]
+
+  if (!product) {
+    return <div>No product found</div>;
+  }
+
+  const { image, title, subtitle, tags } = product;
+
   return (
     <div className="flex bg-gray-100 ml-8 mr-8">
 
       <div className="w-1/5 mr-8 bg-white rounded-lg shadow dark:bg-gray-800 ">
-        <Image width={300} height={500} className='' src={product.image} alt="Magic Bullet NutriBullet" />
-        <div className='font-bold text-lg text-center'>{product.title}</div>
-        <div className='text-gray-400 text-xs text-center pb-8 '>{product.subtitle}</div>
+        <Image width={300} height={500} className='' src={image} alt={subtitle} />
+        <div className='font-bold text-lg text-center'>{title}</div>
+        <div className='text-gray-400 text-xs text-center pb-8 '>{subtitle}</div>
         <hr />
         <div className="flex flex-wrap ">
-
-          {product.tags.map((item, index) =>
-            <div key={index} className='m-1 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500 '>
-              {item}
-            </div>)}
+          {tags.map((item) =>
+            <Tag key={item} item={item} />
+          )}
         </div>
         <hr />
       </div>
